5ea9aafae1edd3cdf98768cf541e908a91278710
ivankprod <houseprotector@mail.ru>
2024-09-17 15:25:25 +0500

    fix(components): smooth-scrollbar scrolling issue

--- components/Scroller/Scroller.tsx
+++ components/Scroller/Scroller.tsx
@@ -3,6 +3,13 @@
 import { useRef, useEffect } from "react";
 import SmoothScrollbar from "smooth-scrollbar";
 import { Scrollbar } from "smooth-scrollbar/interfaces";
+import { Scrollbar as ScrollbarClass } from "smooth-scrollbar/scrollbar";
+
+/* SmoothScrollbar: фиксим баг на горизонтальную прокрутку на мобилках */
+const setMomentum = ScrollbarClass.prototype.setMomentum;
+ScrollbarClass.prototype.setMomentum = function (_, y) {
+	setMomentum.call(this, 0, y);
+};
 
 /** @public */
 export const Scroller: React.FC = () => {
